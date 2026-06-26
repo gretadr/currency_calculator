75e425ae2a389f091df035320ea1dbdee9cf647b
gretadr <dragotigreta@yahoo.com>
2026-06-26 16:01:30 +0300

    Update db.js for production

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,19 @@
 import pg from "pg";
 
-
-const db = new pg.Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
-});
+const isProduction = process.env.NODE_ENV === "production";
+const db = new pg.Pool(
+    isProduction  
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl : { rejectUnauthorized : false }
+    } 
+    : {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_DATABASE,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT
+    }
+);
 
 export default db;
